aabbddc4835e1be82374f1282fec956587cd4965
Ralph Yozzo <fedex1@gmail.com>
2014-06-13 17:24:07 -0400

    Add query

--- bq.queries.sh
+++ bq.queries.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-bq query 'select count(*),avg(length(regexp_replace(voterhistory,"[^;]",""))) elections_voted, year(timestamp(dob)) birthyear from [nys.all_voter_file] group by birthyear order by elections_voted desc limit 1000'
+# bq query 'select count(*),avg(length(regexp_replace(voterhistory,"[^;]",""))) elections_voted, year(timestamp(dob)) birthyear from [nys.all_voter_file] group by birthyear order by elections_voted desc limit 1000'
+bq query --format csv 'select *,year(timestamp(dob)) birthyear from [nys.all_voter_file] order by birthyear limit 1000'
