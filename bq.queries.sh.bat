32c71b59dc127813965c71cae4609c4c3463ac68
unknown <ralph@brooklynmarathon.com>
2014-06-14 10:59:25 -0400

    windows script

--- bq.queries.sh.bat
+++ bq.queries.sh.bat
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+: #!/usr/bin/env bash
 
-# bq query 'select count(*),avg(length(regexp_replace(voterhistory,"[^;]",""))) elections_voted, year(timestamp(dob)) birthyear from [nys.all_voter_file] group by birthyear order by elections_voted desc limit 1000'
-bq query --format csv 'select *,year(timestamp(dob)) birthyear from [nys.all_voter_file] order by birthyear limit 1000'
+: # bq query 'select count(*),avg(length(regexp_replace(voterhistory,"[^;]",""))) elections_voted, year(timestamp(dob)) birthyear from [nys.all_voter_file] group by birthyear order by elections_voted desc limit 1000'
+: bq query --format csv 'select *,year(timestamp(dob)) birthyear from [nys.all_voter_file] order by birthyear limit 1000'
+: bq query "select count(*),sum(length(regexp_replace(voterhistory,'[^;]',''))) elections_voted, RZIP4 from [nys.all_voter_file] group by RZIP4 order by elections_voted desc limit 1000"
+bq query --max_rows 20000 --format csv "select count(*),sum(length(regexp_replace(voterhistory,'[^;]',''))) elections_voted, RZIP5 from [nys.all_voter_file] group by RZIP5 order by elections_voted desc limit 20000"
